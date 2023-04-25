4f1dc4eee921bea2ef26dd9b1f7fcaa41fc8c891
Alexhqolma <hqolma@gmail.com>
2023-04-25 15:54:46 +0300

    Fixed Validation

--- src/main/java/bank/atm/dto/request/UserRequestDto.java
+++ src/main/java/bank/atm/dto/request/UserRequestDto.java
@@ -1,9 +1,15 @@
 package bank.atm.dto.request;
 
+import jakarta.validation.constraints.Size;
+
 import java.util.List;
 
 public class UserRequestDto {
+    @Size(min = 5, max = 20, message
+            = "Login must be between 5 and 20 characters")
     private String login;
+    @Size(min = 8, max = 20, message
+            = "Password must be between 8 and 20 characters")
     private String password;
     private String role;
     private List<Long> accountsId;
--- src/main/java/bank/atm/model/User.java
+++ src/main/java/bank/atm/model/User.java
@@ -20,11 +20,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Size(min = 5, max = 20, message
-            = "Login must be between 5 and 20 characters")
     private String login;
-    @Size(min = 8, max = 20, message
-            = "Password must be between 8 and 20 characters")
     private String password;
     private String role;
     @OneToMany
